e36fd7d957f2448a0deaa9c3fd836390b5e632d3
werkaska3 <weronika3@o2.pl>
2018-10-21 14:41:38 +0200

    jhgfds

--- src/CustomLinkedList.java
+++ src/CustomLinkedList.java
@@ -1,6 +1,14 @@
 public class CustomLinkedList<E> implements CustomList<E> {
 
 
+    private int size;
+    private Node head;
+
+    public CustomLinkedList() {
+        head = new Node(null);
+    }
+
+
     @Override          //to znaczy ze metoda jest przyslonieta
     public E get(int index) {
         return null;
@@ -8,11 +16,40 @@ public class CustomLinkedList<E> implements CustomList<E> {
 
     @Override
     public void add(E e) {
-
+        Node copy = head;
+        System.out.println("Wartosc copy: " + copy);
+        head = new Node(e);
+        System.out.println("Wartosc head: " + head);
+        head.next = copy; //przesuwanie elementu
+        System.out.println("Wartosc next: " + head.next);
+        size++; //zwiekszanie rozmiaru
     }
 
     @Override
     public int size() {
         return 0;
     }
+
+    class Node {
+        private Object data; ///dane
+        private Node next;  //wskaznik a te dane
+
+        //konstruktor
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data= " + data +
+                    ", next= " + next +
+                    '}';
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         //ArrayList - implementacja tablicowa  //dziala szybciej
-        // LinkedList - implementacja   //gdy usuwamy czesciej wybieramy to
+        // LinkedList - implementacja wiazana  //gdy usuwamy czesciej wybieramy to
+
+        CustomList<String> customList = new CustomLinkedList<>();
+
+        customList.add("Jeden");
+        customList.add("Dwa");
+        customList.add("Trzy");
+        customList.add("Cztery");
+        customList.add("Pięć");
+        customList.add("Sześć");
 
 	List<String> list = new LinkedList<>();
 
-    list.add("Jan");
-    list.add("Marcin");
-    list.add("Karol");
+    list.add("Janko");
+    list.add("Marcinko");
+    list.add("Karolko");
 
     for (String l: list){
         System.out.println(l);
